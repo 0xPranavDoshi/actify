ee3ce249bf545c0b522f998b5bf34598751184ef
Dhruv317 <droongta05@gmail.com>
2023-01-21 18:38:18 +0530

    backend work

--- app.py
+++ app.py
@@ -4,11 +4,20 @@ from flask_cors import cross_origin
 import json
 import certifi
 from pymongo import MongoClient
+import os
+import openai
+openai.api_key = "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"
+
 app = Flask(__name__)
 client = MongoClient('mongodb+srv://Actify:Act1fy@cluster0.u87uqzy.mongodb.net/test', tlsCAFile=certifi.where())
 db = client["Main"]
 orgCollection = db["Organisations"]
 initiativeCollection = db["Initiatives"]
+
+def updatePetition(title):
+    initiativeCollection.update_one({"title":title},{"$inc":{"petitionVotes":1}})
+    petitionVotes = initiativeCollection.find_one({"title":title})["petitionVotes"]
+    return petitionVotes
 
 def getInitiatives():
     cursor = initiativeCollection.find({})
@@ -28,6 +37,17 @@ def insertInitiative(name,email,title,description,donationGoal,donationAmount,lo
     print(insertDict)
     initiativeCollection.insert_one(insertDict)
 
+def createDescription(name,location,needs):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Write an appeal for an initiative called {}, located in {}, that needs{}".format(name,location,needs)
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response["choices"][0]["text"]
 #[feasability,technology,implementation, innovation, problem statement ]
 
 @app.route('/signUp', methods=["GET", "POST"])
@@ -72,6 +92,34 @@ def fetchInitiatives():
         except Exception as e:
             print(e)
         
+@app.route('/generateDescription',methods = ["GET","POST"])
+@cross_origin()
+def generateDescription():
+    if request.method == "POST":
+        try: 
+            x = json.loads(request.data.decode("utf-8"))
+            json_data = json.loads(x["body"])
+            to_return = createDescription(json_data["title"],json_data["city"],json_data["needs"])
+            return ({"description":to_return})
+        except Exception as e: 
+            print (e)
+
+@app.route('/updatePetitionVotes',methods = ["GET","POST"])
+@cross_origin()
+def updatePetitionVotes():
+    if request.method == "POST":
+        try: 
+            x = json.loads(request.data.decode("utf-8"))
+            json_data = json.loads(x["body"])
+            petitionVotes = updatePetitionVotes(json_data["title"])
+            return ({"petitionVotes":petitionVotes})
+        except Exception as e: 
+            print (e)
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
